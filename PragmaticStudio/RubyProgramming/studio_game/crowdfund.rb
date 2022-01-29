e7e5fb7e621fb3c9482c1fda887267b444fe2414
Lloyd <zebra05@gmail.com>
2022-01-29 04:48:28 +0200

    Add crowdfund program

---
+++ PragmaticStudio/RubyProgramming/studio_game/crowdfund.rb
@@ -0,0 +1,6 @@
+project1 = "Project ABC"
+project2 = "Project MLN"
+project3 = "Project XYZ"
+
+puts "Project #{project1} has $1000 in funding."
+puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
