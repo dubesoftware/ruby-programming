9ef222ee0a4a7b38ae5f04cb2a25e5e8ffcf8dce
Lloyd <zebra05@gmail.com>
2022-01-30 01:49:47 +0200

    Variables and objects

--- PragmaticStudio/RubyProgramming/studio_game/crowdfund.rb
+++ PragmaticStudio/RubyProgramming/studio_game/crowdfund.rb
@@ -1,6 +1,14 @@
 project1 = "Project ABC"
-project2 = "Project MLN"
-project3 = "Project XYZ"
+funding1 = 1000
 
-puts "Project #{project1} has $1000 in funding."
+project2 = "Project MLN"
+funding2 = 1500
+
+project3 = "Project XYZ"
+funding3 = 2000
+
+puts "Project #{project1} has #{funding1} in funding."
+puts "Project #{project2} has #{funding2} in funding."
+puts "Project #{project3} has #{funding3} in funding."
+
 puts "Projects:\n\t#{project1}\n\t#{project2}\n\t#{project3}"
--- PragmaticStudio/RubyProgramming/studio_game/studio_game.rb
+++ PragmaticStudio/RubyProgramming/studio_game/studio_game.rb
@@ -1,6 +1,25 @@
 name1 = "larry"
+health1 = 60
+puts "#{name1.capitalize} has a health of #{health1}."
+
 name2 = "curly"
+health2 = 125
+puts "#{name2.upcase} has a health of #{health2}."
+health2 = health1
+puts "#{name2.upcase} now has a health of #{health2}."
+
+health1 = 30
+puts "#{name1.capitalize} has a new health of #{health1}."
+puts "#{name2.upcase} still has a health of #{health2}."
+
 name3 = "moe"
+health3 = 100
+puts "#{name3.capitalize} has a health of #{health3}.".center(48, "*")
 
+name4 = "Shemp"
+health = 90
+puts "#{name4.ljust(32, '.')} #{health} health."
 
-puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
+timestamp = Time.new
+formatted_time = timestamp.strftime("%A %d/%m/%Y at %I:%M%p")
+puts "The game started on #{formatted_time}."
