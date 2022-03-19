2500a6c59384bea8220850c55f525c2e8fff4fbe
Lloyd <zebra05@gmail.com>
2022-03-19 04:16:06 +0200

    Use getters and setters created through attr_accessor and attr_reader shortcuts

--- PragmaticStudio/RubyProgramming/studio_game/studio_game.rb
+++ PragmaticStudio/RubyProgramming/studio_game/studio_game.rb
@@ -8,24 +8,24 @@ class Player
   end
   
   def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+    "I'm #{name} with a health of #{health} and a score of #{score}."
   end
   
   def blam
     @health -= 10
-    puts "#{@name} got blammed!"
+    puts "#{name} got blammed!"
   end
   
   def w00t
     @health += 15
-    puts "#{@name} got w00ted!"
+    puts "#{name} got w00ted!"
   end
   
   def score
     @health + @name.length
   end
   
-  def name= new_name
+  def name= (new_name)
     @name = new_name.capitalize
   end
 end
