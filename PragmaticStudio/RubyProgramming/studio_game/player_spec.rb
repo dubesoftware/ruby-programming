ee3844553490771837acd1b307bbb64a1e332a00
Lloyd <zebra05@gmail.com>
2022-04-28 23:16:53 +0200

    Format code

--- PragmaticStudio/RubyProgramming/studio_game/player_spec.rb
+++ PragmaticStudio/RubyProgramming/studio_game/player_spec.rb
@@ -4,7 +4,7 @@ require_relative 'player'
 describe Player do
   
   it 'has a capitalized name' do
-    player = Player.new('larry', 150)
+    player = Player.new("larry", 150)
     
     player.name.should == "Larry" 
   end
