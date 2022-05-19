862c9d343f6184ea425a622e23323927f5ee9909
Lloyd <zebra05@gmail.com>
2022-05-19 04:03:09 +0200

    Add iterator block to cycle through rounds and give players turns

--- PragmaticStudio/RubyProgramming/studio_game/game.rb
+++ PragmaticStudio/RubyProgramming/studio_game/game.rb
@@ -14,11 +14,13 @@ class Game
   
   def play(rounds)
     puts "There are #{@players.size} players in #{title}:"
-    puts @players
     
-    @players.each do |player|
-      GameTurn.take_turn(player)
-      puts player
+    1.upto(rounds) do |round|
+      puts "\nRound: #{round}"
+      @players.each do |player|
+        GameTurn.take_turn(player)
+        puts player
+      end
     end
   end
 end
