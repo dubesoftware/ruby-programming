5484b1bdd5d728cc493eadd71677aa834261d35d
Lloyd <zebra05@gmail.com>
2022-09-01 02:44:51 +0200

    Better naming convention for Player instances

--- PragmaticStudio/RubyProgramming/studio_game/game.rb
+++ PragmaticStudio/RubyProgramming/studio_game/game.rb
@@ -18,38 +18,38 @@ class Game
   end
   
   def total_points
-    @players.reduce(0) { |sum, p| sum + p.points }
+    @players.reduce(0) { |sum, player| sum + player.points }
   end
   
   def print_stats    
     puts "\n#{@title} Statistics:"
     
-    strong_players, wimpy_players = @players.partition { |p| p.strong?  }
+    strong_players, wimpy_players = @players.partition { |player| player.strong?  }
     
     puts "\n#{strong_players.size} strong players:"
-    strong_players.each do |p|
-      print_name_and_health(p)
+    strong_players.each do |player|
+      print_name_and_health(player)
     end
     
     puts "\n#{wimpy_players.size} wimpy players:"
-    wimpy_players.each do |p|
-      print_name_and_health(p)
+    wimpy_players.each do |player|
+      print_name_and_health(player)
     end
     
     puts "\n#{total_points} total points from treasures found"
     
-    @players.each do |p|
-      puts "\n#{p.name}'s point totals:"
+    @players.each do |player|
+      puts "\n#{player.name}'s point totals:"
       p.each_found_treasure do |treasure|
         puts "#{treasure.points} total #{treasure.name} points"
       end
-      puts "#{p.points} grand total points"
+      puts "#{player.points} grand total points"
     end
     
     puts "\n#{@title} High Scores:"
-    @players.sort.each do |p|
-      formatted_name = p.name.ljust(20, '.')
-      puts "#{formatted_name} #{p.score}"
+    @players.sort.each do |player|
+      formatted_name = player.name.ljust(20, '.')
+      puts "#{formatted_name} #{player.score}"
     end
   end
   
