0cdfd324123e5bc1ac7df9065dbf296aba8fdad7
Lloyd <zebra05@gmail.com>
2022-10-20 21:03:42 +0200

    Revert "Interact with the Game"
    This reverts commit ffeebf5e5cc136bcf53675f2839cd2a5b43e0aef.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,6 +1,7 @@
 require_relative 'player'
 require_relative 'game'
 
+
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
@@ -10,20 +11,7 @@ knuckleheads = Game.new("Knuckleheads")
 players.each do |player|
   knuckleheads.add_player(player)
 end
-
-loop do
-  puts "\nHow many game rounds? ('quit' to exit)"
-  answer = gets.chomp.downcase
-  
-  case answer
-  when /^\d+$/
-    knuckleheads.play(answer.to_i) do
-      knuckleheads.total_points >= 2000
-    end
-  when 'quit', 'exit'
-    knuckleheads.print_stats
-    break
-  else
-    puts "Please enter a number or 'quit'"
-  end
+knuckleheads.play(10) do
+  knuckleheads.total_points >= 2000
 end
+knuckleheads.print_stats
