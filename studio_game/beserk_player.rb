939398d65973d06f6b7a76484767fb51685617cc
Lloyd <zebra05@gmail.com>
2022-10-22 01:07:03 +0200

    Remove spec

--- studio_game/beserk_player.rb
+++ studio_game/beserk_player.rb
@@ -4,10 +4,18 @@ class BeserkPlayer < Player
   
   def initialize(name, health)
     super(name, health)
-    @woot_count = 0
+    @w00t_count = 0
   end
   
   def berserk?
-    @woot_count > 5
+    @w00t_count > 5
+  end
+  
+  def w00t
+    super
+    @w00t_count += 1
+    if beserk?
+      puts("Berserker is berserk!")
+    end
   end
 end
