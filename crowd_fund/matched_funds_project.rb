bcb9498156e23039801294d75ec2daeb2d888c23
Lloyd <zebra05@gmail.com>
2022-10-22 23:42:12 +0200

    Add MatchedFundsProject class

---
+++ crowd_fund/matched_funds_project.rb
@@ -0,0 +1,14 @@
+require_relative 'project'
+
+class MatchedFundsProject < Project
+  
+  def initialize(name, initial_funding_amount, target_funding_amount = 0, matching_ratio = 0.5)
+    super(name, initial_funding_amount, target_funding_amount)
+    @matching_ratio = matching_ratio
+  end
+  
+  def match_funds
+    if @current_funding_amount >= (target_funding_amount * 0.5)
+      add_to_funds(target_funding_amount * 0.5)
+    end
+end
