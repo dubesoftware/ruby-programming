a598c878dfb0028d695917addd0499bf0bb518fa
Lloyd <zebra05@gmail.com>
2022-10-29 20:24:49 +0200

    Implicit (default) receiver of method calls is self

--- studio_game/playable.rb
+++ studio_game/playable.rb
@@ -2,12 +2,12 @@ module Playable
   
   def w00t
     @health += 15
-    puts "#{self.name} got w00ted!"
+    puts "#{name} got w00ted!"
   end
   
   def blam
     @health -= 10
-    puts "#{self.name} got blammed!"
+    puts "#{name} got blammed!"
   end
   
   def strong?
