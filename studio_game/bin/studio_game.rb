0b8e16ad0d10e44230f6c5c4f2eba73f2ded679d
Lloyd <zebra05@gmail.com>
2022-10-30 22:54:55 +0200

    Call require_relative with relative path

--- studio_game/bin/studio_game.rb
+++ studio_game/bin/studio_game.rb
@@ -1,7 +1,7 @@
-require_relative 'player'
-require_relative 'clumsy_player'
-require_relative 'berserk_player'
-require_relative 'game'
+require_relative '../lib/studio_game/player'
+require_relative '../lib/studio_game/clumsy_player'
+require_relative '../lib/studio_game/berserk_player'
+require_relative '../lib/studio_game/game'
 
 knuckleheads = StudioGame::Game.new("Knuckleheads")
 knuckleheads.load_players(ARGV.shift || "players.csv")
