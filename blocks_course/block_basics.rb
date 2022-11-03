50a8e00f5e8129760d0518dccfea2557fda66fc6
Lloyd <zebra05@gmail.com>
2022-11-03 04:07:20 +0200

    Use times and upto methods

--- blocks_course/block_basics.rb
+++ blocks_course/block_basics.rb
@@ -1,1 +1,13 @@
-puts "Let's start mastering Ruby blocks!"
+5.times do |n|
+  %w(situp pushup chinup).each do |w|
+    puts "#{n} #{w}"
+  end
+end
+
+puts "".ljust(20, '-')
+
+1.upto(5) do |n|
+  %w(situp pushup chinup).each do |w|
+    puts "#{n} #{w}"
+  end
+end
