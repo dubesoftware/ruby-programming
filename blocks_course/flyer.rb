15e1c981f5756d671147999cea6575bd351d1f4b
Lloyd <zebra05@gmail.com>
2022-11-03 22:59:46 +0200

    Write a block that doubles and prints out prices of desserts

--- blocks_course/flyer.rb
+++ blocks_course/flyer.rb
@@ -42,3 +42,7 @@ puts separator
 promotions = { "United" => 1.5, "Delta" => 2.0, "Lufthansa" => 2.5 }
 promotions.each { |k, v| puts "Earn #{v}x miles by flying #{k}!" }
 puts separator
+
+desserts = { "chocolate" => 1.00, "vanilla" => 0.75, "cinnamon" => 1.25 }
+desserts.each { |k, v| puts "$#{v * 2} for a cup of #{k}." }
+puts separator
