0088dc0d361c09865981f02e2914a9cecb129ce1
Lloyd <zebra05@gmail.com>
2022-11-03 22:07:11 +0200

    Use the each method to print each player's name and score

--- blocks_course/cards.rb
+++ blocks_course/cards.rb
@@ -3,8 +3,10 @@ def separator
 end
 
 cards = %w(Jack Queen King Ace Joker)
+scores = {"Larry" => 10, "Moe" => 8, "Curly" => 12}
 
 cards.shuffle.  each { |c| puts "#{c.upcase} - #{c.length}" }
 separator
 cards.reverse_each { |card| puts "#{card.upcase} - #{card.length}" }
 separator
+scores.each { |k, v| puts "#{k} scored a #{v}!" }
