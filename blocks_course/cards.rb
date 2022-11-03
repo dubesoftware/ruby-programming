2f8b925b865f41a66a167985ea3e6ea599ca6bab
Lloyd <zebra05@gmail.com>
2022-11-03 22:37:13 +0200

    Reformat spacing for readability

--- blocks_course/cards.rb
+++ blocks_course/cards.rb
@@ -6,7 +6,9 @@ scores = {"Larry" => 10, "Moe" => 8, "Curly" => 12}
 
 cards.shuffle.  each { |c| puts "#{c.upcase} - #{c.length}" }
 puts separator
+
 cards.reverse_each { |card| puts "#{card.upcase} - #{card.length}" }
 puts separator
+
 scores.each { |k, v| puts "#{k} scored a #{v}!" }
 puts separator
