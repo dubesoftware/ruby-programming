de488c02a94810a9d4f7cc4eb0407225d6f9895c
Lloyd <zebra05@gmail.com>
2022-11-03 21:59:53 +0200

    Iterate through the cards in the reverse order

--- blocks_course/cards.rb
+++ blocks_course/cards.rb
@@ -1,2 +1,10 @@
+def separator
+  puts "".ljust(20, '-')
+end
+
 cards = %w(Jack Queen King Ace Joker)
-cards.shuffle.each { |c| puts "#{c.upcase} - #{c.length}" }
+
+cards.shuffle.  each { |c| puts "#{c.upcase} - #{c.length}" }
+separator
+cards.reverse_each { |card| puts "#{card.upcase} - #{card.length}" }
+separator
