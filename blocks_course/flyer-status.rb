e32db1b1e929687735f7e2e75c9e19cead3a2502
Lloyd <zebra05@gmail.com>
2022-11-04 20:02:15 +0200

    Add flyer-status.rb file and populate example flyers

---
+++ blocks_course/flyer-status.rb
@@ -0,0 +1,7 @@
+require_relative 'flyer'
+
+flyers = []
+flyers << Flyer.new("Larry", "larry@example.com", 4000, :platinum)
+flyers << Flyer.new("Moe", "moe@example.com", 1000)
+flyers << Flyer.new("Curly", "curly@example.com", 3000, :gold)
+flyers << Flyer.new("Shemp", "shemp@example.com", 2000)
