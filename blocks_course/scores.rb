5ce5d0f55fbfba1d3eaf6e3c5e002504484f63b4
Lloyd <zebra05@gmail.com>
2022-11-04 19:24:09 +0200

    require and call separator method from utilities module

--- blocks_course/scores.rb
+++ blocks_course/scores.rb
@@ -1,4 +1,8 @@
+require_relative 'utilities'
+
+separator = Utilities::separator
 scores = [83, 71, 92, 64, 98, 87, 75, 69]
 
 high_scores = scores.select { |s| s > 80 }
 p high_scores
+puts separator
