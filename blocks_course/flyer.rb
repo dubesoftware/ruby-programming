59ed5ca24127791d8677c904518569bcf9ff4a5a
Lloyd <zebra05@gmail.com>
2022-11-04 19:58:28 +0200

    Add status to Flyer class

--- blocks_course/flyer.rb
+++ blocks_course/flyer.rb
@@ -4,15 +4,17 @@ separator = Utilities::separator
 
 class Flyer
   attr_reader :name, :email, :miles_flown
+  attr_accessor :status
 
-  def initialize(name, email, miles_flown)
+  def initialize(name, email, miles_flown, status = :bronze)
     @name        = name
     @email       = email
     @miles_flown = miles_flown
+    @status      = status
   end
 
   def to_s
-    "#{name} (#{email}): #{miles_flown}"
+    "#{name} (#{email}): #{miles_flown} - #{status}"
   end
 end
 
