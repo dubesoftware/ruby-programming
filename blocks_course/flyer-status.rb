b6dfd69a96d673c3ab09c2a44cb27fd02a7c539f
Lloyd <zebra05@gmail.com>
2022-11-04 20:06:58 +0200

    Find and print frequent flyer customers

--- blocks_course/flyer-status.rb
+++ blocks_course/flyer-status.rb
@@ -9,3 +9,6 @@ flyers << Flyer.new("Moe", "moe@example.com", 1000)
 flyers << Flyer.new("Curly", "curly@example.com", 3000, :gold)
 flyers << Flyer.new("Shemp", "shemp@example.com", 2000)
 
+frequent_flyer_customers = flyers.select { |f| f.miles_flown >= 3000 }
+puts frequent_flyer_customers
+puts separator
