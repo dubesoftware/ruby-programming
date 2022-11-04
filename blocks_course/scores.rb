a9c98a8d2a4e641e3ae9f6a9e41c7c2c06dc91e1
Lloyd <zebra05@gmail.com>
2022-11-04 19:29:36 +0200

    Iterate through the scores and reject those greater than 80

--- blocks_course/scores.rb
+++ blocks_course/scores.rb
@@ -6,3 +6,7 @@ scores = [83, 71, 92, 64, 98, 87, 75, 69]
 high_scores = scores.select { |s| s > 80 }
 p high_scores
 puts separator
+
+low_scores = scores.reject { |s| s > 80 }
+p low_scores
+puts separator
