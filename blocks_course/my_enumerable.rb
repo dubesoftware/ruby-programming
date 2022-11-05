5a2952b8917875ce82317f78e5b774d628a504c3
Lloyd <zebra05@gmail.com>
2022-11-05 22:07:43 +0200

    Implement my_reject method

--- blocks_course/my_enumerable.rb
+++ blocks_course/my_enumerable.rb
@@ -14,4 +14,12 @@ module MyEnumerable
     end
     new_array
   end
+  
+  def my_reject
+    new_array = []
+    each do |value|
+      new_array << value if !(yield(value))
+    end
+    new_array
+  end
 end
