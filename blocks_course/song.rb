b702cbfbcd67ca6447143c55265a393232b66260
Lloyd <zebra05@gmail.com>
2022-11-05 18:35:06 +0200

    Add Playlist class

--- blocks_course/song.rb
+++ blocks_course/song.rb
@@ -15,3 +15,14 @@ end
 song1 = Song.new("Okie From Muskogee", "Merle", 5)
 song2 = Song.new("Ramblin' Man", "Hank", 7)
 song3 = Song.new("Good Hearted Woman", "Waylon", 6)
+
+class Playlist
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+end
