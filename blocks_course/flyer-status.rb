dd67eba06bbf1e1057f34b96f21b114316fe8930
Lloyd <zebra05@gmail.com>
2022-11-05 02:56:48 +0200

    Find and print the top flyer by miles

--- blocks_course/flyer-status.rb
+++ blocks_course/flyer-status.rb
@@ -51,3 +51,7 @@ puts separator
 total_bronze_km = flyers.select { |f| f.status == :bronze }.map { |f| f.miles_flown * 1.6 }.reduce(0, :+)
 puts "Total distance flown by bronze flyers in kilometres: #{total_bronze_km}"
 puts separator
+
+top_flyer = flyers.max_by { |f| f.miles_flown }
+puts "Flyer with the most miles: #{top_flyer}"
+puts separator
