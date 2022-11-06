3472b93d94e6d064028c0f3a5bba8d23df53c427
Lloyd <zebra05@gmail.com>
2022-11-06 23:28:57 +0200

    Implement with_expectation method

--- blocks_course/debugging.rb
+++ blocks_course/debugging.rb
@@ -4,6 +4,10 @@ def with_debugging
   puts "Result was #{result}"
 end
 
+def with_expectation(expected_value)
+  yield == expected_value ? puts "The block passed" : puts "The block failed"
+end
+
 with_debugging do
   magic_number = (23 - Time.now.hour) * Math::PI
 end
