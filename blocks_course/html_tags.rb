edc935ee64657eae6b2aedf6e40806692c7ad662
Lloyd <zebra05@gmail.com>
2022-11-06 22:58:01 +0200

    Fix tag method output and printing

--- blocks_course/html_tags.rb
+++ blocks_course/html_tags.rb
@@ -3,24 +3,35 @@ require_relative 'utilities'
 separator = Utilities::separator
 
 def h1
-  content = yield
-  "<h1>#{content}</h1>"
+  print "<h1>"
+  print yield
+  print "</h1>"
 end
 
 def h2
-  content = yield
-  "<h2>#{content}</h2>"
+  print "<h2>"
+  print yield
+  print "</h2>"
 end
 
 def tag(name)
-  content = yield
-  "<#{name}>#{content}</#{name}>"
+  print "<#{name}>"
+  print yield
+  puts "</#{name}>"
 end
 
 puts "Generate h1 tag:"
-puts tag(:h1) { "Breaking News!" }
+tag(:h1) { "Breaking News!" }
 puts separator
 
 puts "Generate h2 tag:"
-puts tag(:h2) { "Massive Ruby Discovered" }
+tag(:h2) { "Massive Ruby Discovered" }
+puts separator
+
+puts "Generate nested HTML elements:"
+tag(:ul) do
+  tag(:li) { "It sparkles!"}
+  tag(:li) { "It shines!"}
+  tag(:li) { "It mesmerizes!"}
+end
 puts separator
