2ae5a9fe200309c316cf33f523a117629659c090
Lloyd <zebra05@gmail.com>
2022-11-06 22:30:07 +0200

    Require and call separator method

--- blocks_course/html_tags.rb
+++ blocks_course/html_tags.rb
@@ -1,6 +1,12 @@
+require_relative 'utilities'
+
+separator = Utilities::separator
+
 def h1
   content = yield
   "<h1>#{content}</h1>"
 end
 
+puts "Generate h1 tag:"
 puts h1 { "Breaking news!" }
+puts separator
