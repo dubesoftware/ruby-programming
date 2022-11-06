5d24b847415ad2100023801f6d1ef8f5ec8f5e2e
Lloyd <zebra05@gmail.com>
2022-11-06 22:38:24 +0200

    Define generate_tag method

--- blocks_course/html_tags.rb
+++ blocks_course/html_tags.rb
@@ -2,6 +2,11 @@ require_relative 'utilities'
 
 separator = Utilities::separator
 
+def generate_tag(tag)
+  content = yield
+  "<#{tag}>#{content}</#{tag}>"
+end
+
 def h1
   content = yield
   "<h1>#{content}</h1>"
