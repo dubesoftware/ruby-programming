627e52d5bf7b79750f0e5f9cf47fcb6034c7d235
Lloyd <zebra05@gmail.com>
2022-11-07 23:43:32 +0200

    Decrease sleep to allow continued execution

--- blocks_course/time_cop.rb
+++ blocks_course/time_cop.rb
@@ -22,7 +22,15 @@ puts separator
 puts "Example timeout with with duration 2.0 and sleep 3.0:"
 puts separator
 Timeout.timeout(2.0) do
-  sleep 3.0
+  sleep 0.3
+  puts "That was refreshing..."
+end
+puts separator
+
+puts "Call timeout with sleep shorter than timeout duration:"
+puts separator
+try_with_timeout(2.0) do
+  sleep 1.0
   puts "That was refreshing..."
 end
 puts separator
