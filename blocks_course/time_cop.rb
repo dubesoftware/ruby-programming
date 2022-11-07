687d3aa78455b592595c4dfba810f1ddd278a1ea
Lloyd <zebra05@gmail.com>
2022-11-07 23:33:48 +0200

    Format ouput with separator

--- blocks_course/time_cop.rb
+++ blocks_course/time_cop.rb
@@ -1,6 +1,12 @@
 require 'timeout'
+require_relative 'utilities'
 
+separator = Utilities::separator
+
+puts "Example timeout with with duration 2.0 and sleep 1.0:"
+puts separator
 Timeout.timeout(2.0) do
   sleep 1.0
   puts "That was refreshing..."
 end
+puts separator
