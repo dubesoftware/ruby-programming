c3851511cb738e354e65deb05f4bbd0d6970874d
Lloyd <zebra05@gmail.com>
2022-11-07 23:05:00 +0200

    Add DatabaseDriver class

---
+++ blocks_course/database_driver.rb
@@ -0,0 +1,25 @@
+class DatabaseDriver
+  def initialize(database, user, password)
+    @database = database
+    @user = user
+    @password = password
+    @connected = false
+  end
+
+  def connect
+    # connects to database
+    @connected = true
+    puts "Connected to #{@database} as #{@user}."
+  end
+
+  def disconnect
+    # disconnects from database
+    puts "Disconnected."
+  end
+
+  def execute(sql)
+    raise "Not connected!" unless @connected
+    puts "Executing #{sql}..."
+    # executes SQL
+  end
+end
