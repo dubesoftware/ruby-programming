bf1431cb6c67e51ccc6f5e767896dff76c7000fc
Lloyd <zebra05@gmail.com>
2022-11-08 00:47:47 +0200

    Restore game file

---
+++ studio_game/studio_game.rb
@@ -0,0 +1,29 @@
+require_relative 'player'
+require_relative 'game'
+
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
+players = [player1, player2, player3]
+
+knuckleheads = Game.new("Knuckleheads")
+players.each do |player|
+  knuckleheads.add_player(player)
+end
+
+loop do
+  puts "\nHow many game rounds? ('quit' to exit)"
+  answer = gets.chomp.downcase
+  
+  case answer
+  when /^\d+$/
+    knuckleheads.play(answer.to_i) do
+      knuckleheads.total_points >= 2000
+    end
+  when 'quit', 'exit'
+    knuckleheads.print_stats
+    break
+  else
+    puts "Please enter a number or 'quit'"
+  end
+end
