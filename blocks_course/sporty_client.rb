9415ca34f7b28b8c1864804f6aba9aec30787188
Lloyd <zebra05@gmail.com>
2022-11-07 22:46:07 +0200

    Add SportyClient class

---
+++ blocks_course/sporty_client.rb
@@ -0,0 +1,24 @@
+class SportyClient
+  def post(message)
+    raise "Not signed in!" unless @user
+    # post message to remote service
+    puts "#{@user} posted message: #{message}"
+  end
+
+  def timeline
+    raise "Not signed in!" unless @user
+    # get timeline from remote service
+    puts "#{@user} fetched timeline..."
+  end
+
+  def sign_in(user)
+    # sign in to remote service
+    @user = user
+    puts "#{user} signed in."
+  end
+
+  def sign_out(user)
+    # sign out of remote service
+    puts "#{user} signed out!"
+  end
+end
