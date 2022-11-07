2d47481f112fa916879ed98a734742c9606d97f0
Lloyd <zebra05@gmail.com>
2022-11-07 23:40:38 +0200

    Define try_with_timeout method

--- blocks_course/time_cop.rb
+++ blocks_course/time_cop.rb
@@ -1,6 +1,14 @@
 require 'timeout'
 require_relative 'utilities'
 
+def try_with_timeout(duration)  
+  begin
+    yield
+  rescue
+    puts "Took too long!"
+  end
+end
+
 separator = Utilities::separator
 
 puts "Example timeout with with duration 2.0 and sleep 1.0:"
